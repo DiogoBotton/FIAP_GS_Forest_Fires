a4c7090c57d63582363fc217930090c9696e157f
DiogoBotton <diogo.luo@gmail.com>
2025-06-02 23:45:21 -0300

    API Realizada, falta testar se está tudo certo quando sobe a imagem no docker

---
+++ src/api/enums.py
@@ -0,0 +1,23 @@
+from enum import Enum
+    
+class Mes(Enum):
+    January = 1
+    February = 2
+    March = 3
+    April = 4
+    May = 5
+    June = 6
+    July = 7
+    August = 8
+    September = 9
+    October = 10
+    November = 11
+    December = 12
+
+class Bioma(Enum):
+    Amazonia = 1
+    Cerrado = 2
+    Caatinga = 3
+    Pantanal = 4
+    Pampa = 5
+    MataAtlantica = 6
